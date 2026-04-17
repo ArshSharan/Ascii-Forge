6a0c4e8f949af741d276f9f71e1decd6d96d524b
ArshSharan <arshsharan06@gmail.com>
2026-04-18 00:56:23 +0530

    Added colored ASCII rendering using ANSI escape codes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
 from image_loader import load_image
-from processor import resize, toGrayscale
+from processor import resize
 from ascii_mapper import map_pixels_to_ascii
-from renderer import format_ascii
+from renderer import format_ascii, render_color_ascii
 
-image = load_image("../assets/Image.jpg")
-image = resize(image)
-image = toGrayscale(image)
+IMAGE_PATH = "../assets/Image2.jpg"
 
-ascii_str = map_pixels_to_ascii(image)
-ascii_img = format_ascii(ascii_str, image.width)
+def run_grayscale():
+    image = load_image(IMAGE_PATH)
+    image = resize(image)
+    image = image.convert("L")
 
-print(ascii_img)
+    ascii_str = map_pixels_to_ascii(image)
+    ascii_img = format_ascii(ascii_str, image.width)
+
+    print(ascii_img)
+
+
+def run_color():
+    image = load_image(IMAGE_PATH)
+    image = resize(image)
+
+    render_color_ascii(image)
+
+
+if __name__ == "__main__":
+    mode = input("Choose mode (gray/color): ").strip().lower()
+
+    if mode == "color":
+        run_color()
+    else:
+        run_grayscale()
--- src/renderer.py
+++ src/renderer.py
@@ -2,3 +2,26 @@ def format_ascii(ascii_str, width):
     return "\n".join(
         [ascii_str[i:i + width] for i in range(0, len(ascii_str), width)]
     )
+
+# Rendering colored ASCII directly in terminal using ANSI escape codes
+def render_color_ascii(image):
+    pixels = list(image.getdata())
+    width = image.width
+
+    ASCII_CHARS = "@%#*+=-:. "
+
+    for i in range(0, len(pixels), width):
+        row = pixels[i:i + width]
+
+        for r, g, b in row:
+            # Convert to grayscale (for choosing ASCII char)
+            gray = int(0.299*r + 0.587*g + 0.114*b)
+
+            # Map grayscale to ASCII
+            index = int(gray / 255 * (len(ASCII_CHARS) - 1))
+            char = ASCII_CHARS[index]
+
+            # Print with color
+            print(f"\033[38;2;{r};{g};{b}m{char}\033[0m", end="")
+
+        print()
